c385873c7cc3c6163b4ce2775f19e5279b2072fa
Erhan Emre Eroğlu <emeroglu@ku.edu.tr>
2019-03-23 18:48:03 +0300

    initial commit

---
+++ Screenshot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing.Imaging;
+using System.Threading;
+
+namespace Screenshot
+{
+    class Program
+    {
+
+        private static int Count { get; set; }
+
+        static void Main(string[] args)
+        {
+            Count = 1;
+
+            TimerTask();
+        }
+
+        private static void TimerTask()
+        {
+            if (Count != 10)
+            {
+                ScreenCapture sc = new ScreenCapture();
+                sc.CaptureScreenToFile(@"C:\Users\Erhan Emre Eroğlu\Desktop\Screenshots\Shot_" + Count + ".png", ImageFormat.Png);
+
+                Count++;
+
+                Thread.Sleep(1000);
+
+                TimerTask();
+            }
+        }
+    }
+}
